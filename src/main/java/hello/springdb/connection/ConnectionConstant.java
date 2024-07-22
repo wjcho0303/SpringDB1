bd0aec89fb308378bbe3477bbc2f6319ea7aa1f9
wjcho <wj930303@gmail.com>
2024-07-22 12:17:46 +0900

    Fix: 오타 수정

--- src/main/java/hello/springdb/connection/ConnectionConstant.java
+++ src/main/java/hello/springdb/connection/ConnectionConstant.java
@@ -1,7 +1,7 @@
 package hello.springdb.connection;
 
 public abstract class ConnectionConstant {  // 상수만 다루므로 객체를 생성하면 안 되기 때문에 abstract
-    private static final String URL = "jdbc:h2:tcp://localhost/~/test";
-    private static final String USERNAME = "sa";
-    private static final String PASSWORD = "";
+    public static final String URL = "jdbc:h2:tcp://localhost/~/test";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "";
 }
