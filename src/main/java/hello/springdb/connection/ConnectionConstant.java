a5ea0b9ceacf5a01f03a781ea7c66945f0c6b82c
wjcho <wj930303@gmail.com>
2024-07-22 12:16:46 +0900

    Feat: 연결을 위한 상수 생성

---
+++ src/main/java/hello/springdb/connection/ConnectionConstant.java
@@ -0,0 +1,7 @@
+package hello.springdb.connection;
+
+public abstract class ConnectionConstant {
+    private static final String URL = "jdbc:h2:tcp://localhost/~/test";
+    private static final String USERNAME = "sa";
+    private static final String PASSWORD = "";
+}
