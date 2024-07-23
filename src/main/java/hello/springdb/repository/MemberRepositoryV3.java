efc2fb3eed2242020f00a4b6bbfd5c2f4cb56bff
wjcho <wj930303@gmail.com>
2024-07-24 01:12:34 +0900

    Refactor: 파라미터를 통한 Connection 전달 방식 삭제

--- src/main/java/hello/springdb/repository/MemberRepositoryV3.java
+++ src/main/java/hello/springdb/repository/MemberRepositoryV3.java
@@ -73,37 +73,6 @@ public class MemberRepositoryV3 {
         }
     }
 
-    public Member findById(Connection conn, String memberId) throws SQLException {
-        String sql = "select * from member where member_id = ?";
-
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-
-        try {
-            pstmt = conn.prepareStatement(sql);
-            pstmt.setString(1, memberId);
-
-            rs = pstmt.executeQuery();
-
-            if (rs.next()) {
-                Member member = new Member();
-                member.setMemberId(rs.getString("member_id"));
-                member.setMoney(rs.getInt("money"));
-
-                return member;
-            } else {
-                throw new NoSuchElementException("member not found memberId = " + memberId);
-            }
-        } catch (SQLException e) {
-            log.error("db error", e);
-            throw e;
-        } finally {
-            // Connection 닫는 것을 Service에 위임한다
-            JdbcUtils.closeResultSet(rs);
-            JdbcUtils.closeStatement(pstmt);
-        }
-    }
-
     public void update(String memberId, int money) throws SQLException {
         String sql = "update member set money = ? where member_id = ?";
 
@@ -123,27 +92,6 @@ public class MemberRepositoryV3 {
             throw e;
         } finally {
             close(conn, pstmt, null);
-        }
-    }
-
-    public void update(Connection conn, String memberId, int money) throws SQLException {
-        String sql = "update member set money = ? where member_id = ?";
-
-        PreparedStatement pstmt = null;
-
-        try {
-            pstmt = conn.prepareStatement(sql);
-            pstmt.setInt(1, money);
-            pstmt.setString(2, memberId);
-
-            int resultSize = pstmt.executeUpdate();
-            log.info("resultSize = {}", resultSize);
-        } catch (SQLException e) {
-            log.error("db error", e);
-            throw e;
-        } finally {
-            // Connection 닫는 것을 Service에 위임한다
-            JdbcUtils.closeStatement(pstmt);
         }
     }
 
@@ -169,6 +117,7 @@ public class MemberRepositoryV3 {
     private void close(Connection conn, Statement stmt, ResultSet rs) {
         JdbcUtils.closeResultSet(rs);
         JdbcUtils.closeStatement(stmt);
+        // 트랜잭션 동기화를 사용하려면 DataSourceUtils를 사용해야 한다.
         DataSourceUtils.releaseConnection(conn, dataSource);
     }
 
