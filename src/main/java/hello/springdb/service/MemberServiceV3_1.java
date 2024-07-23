44f61a7b2b1030ee6955d8c03aec594793025de9
wjcho <wj930303@gmail.com>
2024-07-24 01:34:48 +0900

    Refactor: Connection 관리를 모두 트랜잭션 매니저에 위임

--- src/main/java/hello/springdb/service/MemberServiceV3_1.java
+++ src/main/java/hello/springdb/service/MemberServiceV3_1.java
@@ -1,14 +1,13 @@
 package hello.springdb.service;
 
 import hello.springdb.domain.Member;
-import hello.springdb.repository.MemberRepositoryV2;
+import hello.springdb.repository.MemberRepositoryV3;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
-import java.sql.Connection;
 import java.sql.SQLException;
 
 /**
@@ -19,7 +18,7 @@ import java.sql.SQLException;
 public class MemberServiceV3_1 {
 
     private final PlatformTransactionManager transactionManager;
-    private final MemberRepositoryV2 memberRepository;
+    private final MemberRepositoryV3 memberRepository;
 
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
 
@@ -27,43 +26,21 @@ public class MemberServiceV3_1 {
         TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
 
         try {
-            // 수동커밋 모드 설정 = 트랜잭션 시작
-            conn.setAutoCommit(false);
-
-            // 비즈니스 로직 수행: 파라미터에 Connection 전달
-            accountTransferBizLogic(conn, toId, money, fromId);
-
-            // 성공 시 커밋
-            conn.commit();
+            accountTransferBizLogic(toId, money, fromId);
+            transactionManager.commit(status);
         } catch (Exception e) {
-            // 실패 시 롤백
-            conn.rollback();
+            transactionManager.rollback(status);
             throw new IllegalStateException(e);
-        } finally {
-            // Connection 릴리즈
-            release(conn);
         }
     }
 
-    private void accountTransferBizLogic(Connection conn, String toId, int money, String fromId) throws SQLException {
-        Member fromMember = memberRepository.findById(conn, fromId);
-        Member toMember = memberRepository.findById(conn, toId);
+    private void accountTransferBizLogic(String toId, int money, String fromId) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
 
-        memberRepository.update(conn, fromId, fromMember.getMoney() - money);
+        memberRepository.update(fromId, fromMember.getMoney() - money);
         validate(toMember); // 학습을 위한 의도적 예외 발생 코드
-        memberRepository.update(conn, toId, toMember.getMoney() + money);
-    }
-
-    private static void release(Connection conn) {
-        if (conn != null) {
-            try {
-                // 커넥션 풀을 고려하여, 자동커밋 모드로 다시 원복시킨 후 close 해야 한다.
-                conn.setAutoCommit(true);
-                conn.close();
-            } catch (Exception e) {
-                log.info("error", e);
-            }
-        }
+        memberRepository.update(toId, toMember.getMoney() + money);
     }
 
     // toMember의 Id를 "ex"로 해놓고 예외 터트리기
