3c0b57932f6018474ace8ee870d4ae9e7ff88acf
wjcho <wj930303@gmail.com>
2024-07-24 13:42:26 +0900

    Feat: 트랜잭션 템플릿 적용을 위한 파일 복붙

---
+++ src/main/java/hello/springdb/service/MemberServiceV3_2.java
@@ -0,0 +1,52 @@
+package hello.springdb.service;
+
+import hello.springdb.domain.Member;
+import hello.springdb.repository.MemberRepositoryV3;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - 트랜잭션 매니저
+ */
+@RequiredArgsConstructor
+@Slf4j
+public class MemberServiceV3_2 {
+
+    private final PlatformTransactionManager transactionManager;
+    private final MemberRepositoryV3 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+
+        // 트랜잭션 시작
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+        try {
+            accountTransferBizLogic(toId, money, fromId);
+            transactionManager.commit(status);
+        } catch (Exception e) {
+            transactionManager.rollback(status);
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private void accountTransferBizLogic(String toId, int money, String fromId) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validate(toMember); // 학습을 위한 의도적 예외 발생 코드
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    // toMember의 Id를 "ex"로 해놓고 예외 터트리기
+    private static void validate(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체 중 예외 발생");
+        }
+    }
+}
