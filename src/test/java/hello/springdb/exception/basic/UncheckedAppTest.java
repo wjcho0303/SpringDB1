f83d578be6c7efbd6c6b03305075197dec02fdc9
wjcho <wj930303@gmail.com>
2024-07-25 18:25:55 +0900

    Feat: 런타임 예외 테스트 파일 복붙 생성

---
+++ src/test/java/hello/springdb/exception/basic/UncheckedAppTest.java
@@ -0,0 +1,47 @@
+package hello.springdb.exception.basic;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.net.ConnectException;
+import java.sql.SQLException;
+
+public class UncheckedAppTest {
+
+    @Test
+    void checked() {
+        Controller controller = new Controller();
+        Assertions.assertThatThrownBy(() -> controller.request())
+                .isInstanceOf(SQLException.class);
+    }
+
+    static class Controller {
+        Service service = new Service();
+
+        public void request() throws SQLException, ConnectException {
+            service.logic();
+        }
+    }
+
+    static class Service {
+        Repository repository = new Repository();
+        NetworkClient networkClient = new NetworkClient();
+
+        public void logic() throws SQLException, ConnectException {
+            repository.callRepository();
+            networkClient.callNetworkClient();
+        }
+    }
+
+    static class NetworkClient {
+        public void callNetworkClient() throws ConnectException {
+            throw new ConnectException("연결 실패");
+        }
+    }
+
+    static class Repository {
+        public void callRepository() throws SQLException {
+            throw new SQLException("ex");
+        }
+    }
+}
