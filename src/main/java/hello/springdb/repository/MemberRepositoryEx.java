a83d41d6c6cb226e356c26d1046369d6525ecfb3
wjcho <wj930303@gmail.com>
2024-07-25 20:42:21 +0900

    Feat: 특정 기술에 의존하는 인터페이스

---
+++ src/main/java/hello/springdb/repository/MemberRepositoryEx.java
@@ -0,0 +1,12 @@
+package hello.springdb.repository;
+
+import hello.springdb.domain.Member;
+
+import java.sql.SQLException;
+
+public interface MemberRepositoryEx {
+    Member save(Member member) throws SQLException;
+    Member findById(String memberId) throws SQLException;
+    void update(String memberId, int money) throws SQLException;
+    void delete(String memberId) throws SQLException;
+}
