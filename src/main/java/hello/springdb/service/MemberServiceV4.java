de90a2a706884a19b3e233e5a597b48f08fe6126
wjcho <wj930303@gmail.com>
2024-07-25 21:08:24 +0900

    Feat: MemberServiceV4Test 파일 복붙 생성

--- src/main/java/hello/springdb/service/MemberServiceV4.java
+++ src/main/java/hello/springdb/service/MemberServiceV4.java
@@ -5,8 +5,6 @@ import hello.springdb.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.SQLException;
-
 /**
  * 예외 누수 문제 해결
  * SQLException 제거
