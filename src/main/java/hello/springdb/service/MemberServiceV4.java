25b9d19b962d3dc6989912817a1ff494599efadd
wjcho <wj930303@gmail.com>
2024-07-25 21:03:28 +0900

    MemberServiceV4 복붙 생성

---
+++ src/main/java/hello/springdb/service/MemberServiceV4.java
@@ -0,0 +1,42 @@
+package hello.springdb.service;
+
+import hello.springdb.domain.Member;
+import hello.springdb.repository.MemberRepositoryV3;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - @Transactional을 이용한 AOP 적용
+ */
+@Slf4j
+@Transactional
+public class MemberServiceV4 {
+
+    private final MemberRepositoryV3 memberRepository;
+
+    public MemberServiceV4(MemberRepositoryV3 memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        accountTransferBizLogic(toId, money, fromId);
+    }
+
+    private void accountTransferBizLogic(String toId, int money, String fromId) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validate(toMember); // 학습을 위한 의도적 예외 발생 코드
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    // toMember의 Id를 "ex"로 해놓고 예외 터트리기
+    private static void validate(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체 중 예외 발생");
+        }
+    }
+}
