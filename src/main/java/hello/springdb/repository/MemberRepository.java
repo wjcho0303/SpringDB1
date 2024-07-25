f70019113f65806cf627886f5f605f04e846154a
wjcho <wj930303@gmail.com>
2024-07-25 20:23:20 +0900

    Feat: MemberRepository 인터페이스 생성

---
+++ src/main/java/hello/springdb/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package hello.springdb.repository;
+
+import hello.springdb.domain.Member;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Member findById(String memberId);
+    void update(String memberId, int money);
+    void delete(String memberId);
+}
