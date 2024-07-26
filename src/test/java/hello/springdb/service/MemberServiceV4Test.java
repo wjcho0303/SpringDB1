d0cbb9442c5bf69765e4d2d7619009caec072d0f
wjcho <wj930303@gmail.com>
2024-07-26 22:53:09 +0900

    Refactor: ServiceV4Test 주입 구현체를 RepositoryV4_2에서 V5로 변경

--- src/test/java/hello/springdb/service/MemberServiceV4Test.java
+++ src/test/java/hello/springdb/service/MemberServiceV4Test.java
@@ -4,6 +4,7 @@ import hello.springdb.domain.Member;
 import hello.springdb.repository.MemberRepository;
 import hello.springdb.repository.MemberRepositoryV4_1;
 import hello.springdb.repository.MemberRepositoryV4_2;
+import hello.springdb.repository.MemberRepositoryV5;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
@@ -44,7 +45,7 @@ class MemberServiceV4Test {
 
         @Bean
         MemberRepository memberRepository() {
-            return new MemberRepositoryV4_2(dataSource);
+            return new MemberRepositoryV5(dataSource);
         }
 
         @Bean
