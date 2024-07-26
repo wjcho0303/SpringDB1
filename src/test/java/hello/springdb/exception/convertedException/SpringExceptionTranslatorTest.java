525ee90b00bee6b9d40eb01aed82bbca8269f9b3
wjcho <wj930303@gmail.com>
2024-07-26 20:20:27 +0900

    Feat: 런타임 예외로 변환하여 예외처리+스프링 예외 추상화 활용

---
+++ src/test/java/hello/springdb/exception/convertedException/SpringExceptionTranslatorTest.java
@@ -0,0 +1,64 @@
+package hello.springdb.exception.convertedException;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.BadSqlGrammarException;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
+
+import javax.sql.DataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import static hello.springdb.connection.ConnectionConstant.*;
+import static org.assertj.core.api.Assertions.*;
+
+@Slf4j
+public class SpringExceptionTranslatorTest {
+
+    DataSource dataSource;
+
+    @BeforeEach
+    void init() {
+        dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+    }
+
+    @Test
+    void sqlExceptionErrorCode() {
+        String sql = "select bad grammar example";
+
+        try {
+            Connection conn = dataSource.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.executeQuery();
+        } catch (SQLException e) {
+            assertThat(e.getErrorCode()).isEqualTo(42000);
+            int errorCode = e.getErrorCode();
+            log.info("errorCode = {}", errorCode);
+            log.info("error", e);
+        }
+    }
+
+    @Test
+    void springExceptionTranslator() {
+        String sql = "select bad grammar example";
+
+        try {
+            Connection conn = dataSource.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.executeQuery();
+        } catch (SQLException e) {
+            SQLErrorCodeSQLExceptionTranslator exTranslator = new SQLErrorCodeSQLExceptionTranslator(dataSource);
+            DataAccessException resultEx = exTranslator.translate("select", sql, e);
+            log.info("resultEx = ", resultEx);
+
+            assertThat(e.getErrorCode()).isEqualTo(42000);
+            assertThat(resultEx.getClass()).isEqualTo(BadSqlGrammarException.class);
+        }
+    }
+}
